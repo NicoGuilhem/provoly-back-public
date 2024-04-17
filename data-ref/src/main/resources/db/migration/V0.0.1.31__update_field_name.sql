c18012ce91d7c0fb35e63118c7aca39031fafb7f
Damien LG <10082408+superdaminou@users.noreply.github.com>
2024-04-17 14:45:33 +0200

    feat: rename some fields names to be more expressive (#243)
    link #239

---
+++ data-ref/src/main/resources/db/migration/V0.0.1.31__update_field_name.sql
@@ -0,0 +1,7 @@
+UPDATE field SET name = 'Multi-lignes (Lambert93)' WHERE id ='9baf57c5-8b60-4033-b28d-911a48618d31';
+
+UPDATE field SET name = 'Multi-polygones (Lambert93)' WHERE id='a51ef1d2-c5f7-448a-9563-851214e60b4b';
+
+UPDATE field SET name = 'Multi-polygones (WGS84)' WHERE id = 'd6fd71ce-ec71-4d9b-8abb-2d83e0df3b6a';
+
+UPDATE field SET name = 'Multi-lignes (WGS84)' WHERE id = 'eafbffab-7452-4ba3-a686-8bdaeac436b8';
